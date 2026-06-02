d3a2da37281148af36793bb18f879bf92fc96f61
Purus <purusottampaswan925@gmail.com>
2026-06-02 11:56:18 +0530

    add components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,22 @@
 import './App.css'
-import { useState, useEffect } from 'react'
 
 const App = () => {
 
-  const[message, setMessage] = useState("")
-
-  useEffect(() => {
-    fetch("http://localhost:5000/v1/api/message")
-    .then((res) => res.json())
-    .then((data) => setMessage(data.message))
-  }, [])
-    
   return( 
- <div>
-  <h1 className = "text-3xl text-red-700 p-4" > 
-   Purus Gemini
-    </h1>
-  <h2 className = "text-2xl text-blue-600 p-4" >
-     Welcome to the App
-     </h2>
-     <h2 className = "text-lg text-red-500 p-4" >
-       data: {message}
-     </h2>
-  </div>
+    <div className='grid grid-cols-6 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-950 border-1 border-zinc-700 '>
+       sidebar
+      </div>
+      <div className='col-span-5 bg-zinc-950'>
+       <div className='container h-110'>
+
+       </div>
+       <div className="w-1/2 text-white m-auto rounded-2xl">
+        <input type="text" placeholder="Ask me anything" />
+        <button>Ask</button>
+       </div>
+      </div>
+    </div>
   )
 }
 
