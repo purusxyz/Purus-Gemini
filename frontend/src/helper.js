3aa35bb61bcef4972be5f772fbb84edf8038d60f
Purus <purusottampaswan925@gmail.com>
2026-06-22 15:50:21 +0530

    refactor: update styling and restructure components

--- frontend/src/helper.js
+++
@@ -1,7 +0,0 @@
-export function checkHeading(str){
-    return /^(\*)(\*)(.*)\*$/.test(str)
-}
-
-export function replaceHeadingStarts(str){
-    return str.replace(/^(\*)(\*)|(\*)$/g,'')
-}
---
+++ frontend/src/helper.ts
@@ -0,0 +1,7 @@
+export function checkHeading(str: string): boolean {
+    return /^(\*)(\*)(.*)\*$/.test(str)
+}
+
+export function replaceHeadingStarts(str: string): string {
+    return str.replace(/^(\*)(\*)|(\*)$/g, '')
+}
